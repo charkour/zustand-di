69af29044192d9827f11f1214fa0df219a78724e
ck-tekton <33156025+charkour@users.noreply.github.com>
2022-12-27 11:06:12 -0500

    simplify

--- src/index.ts
+++ src/index.ts
@@ -4,25 +4,15 @@ import {
   useRef,
   createElement,
 } from "react";
-import { StoreApi, useStore as useZustandStore } from "zustand";
+import { type StoreApi, useStore as useZustandStore } from "zustand";
 
-export const createContext = <
-  State,
-  Store extends StoreApi<State> = StoreApi<State>
->() => {
-  const StoreContext = createReactContext<Store | null>(null);
-  const useStore = <T>(
-    selector: (state: State) => T,
-    equalityFn?: (left: T, right: T) => boolean
-  ) => {
-    const store = useContext(StoreContext);
-    if (!store) {
-      throw new Error(
-        "useProductListStore must be used within a ProductListProvider"
-      );
-    }
-    return useZustandStore(store, selector, equalityFn);
-  };
+type ExtractState<Store> = Store extends { getState: () => infer T }
+  ? T
+  : never;
+
+// Inspired from: https://github.com/pmndrs/zustand/blob/main/src/context.ts
+export const createContext = <Store extends StoreApi<unknown>>() => {
+  const StoreContext = createReactContext<Store | undefined>(undefined);
 
   const Provider: React.FC<{
     createStore: () => Store;
@@ -31,6 +21,20 @@ export const createContext = <
     const store = useRef(createStore()).current;
     return createElement(StoreContext.Provider, { value: store }, children);
   };
+
+  const useStore = <StateSlice = ExtractState<Store>>(
+    selector: (state: ExtractState<Store>) => StateSlice,
+    equalityFn?: (left: StateSlice, right: StateSlice) => boolean
+  ) => {
+    const store = useContext(StoreContext);
+    if (!store) {
+      throw new Error(
+        "Seems like you have not used zustand provider as an ancestor"
+      );
+    }
+    return useZustandStore(store, selector, equalityFn);
+  };
+
   return {
     Provider,
     useStore,
