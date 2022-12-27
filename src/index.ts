086d54af71a8f5ab9d839dd7766495c079c7f0eb
ck-tekton <33156025+charkour@users.noreply.github.com>
2022-12-27 10:51:10 -0500

    shave bytes

--- src/index.ts
+++ src/index.ts
@@ -1,28 +1,38 @@
-import { createContext as createReactContext, useContext, useRef, createElement } from 'react';
-import { StoreApi, useStore as useZustandStore } from 'zustand';
+import {
+  createContext as createReactContext,
+  useContext,
+  useRef,
+  createElement,
+} from "react";
+import { StoreApi, useStore as useZustandStore } from "zustand";
 
-export const createContext = <State, Store extends StoreApi<State> = StoreApi<State>>() => {
-    const StoreContext = createReactContext<Store | null>(null);
-    const useStore = <T,>(
-        selector: (state: State) => T = (state) => state as unknown as T,
-        equalityFn: (left: T, right: T) => boolean = Object.is
-    ) => {
-        const store = useContext(StoreContext);
-        if (!store) {
-            throw new Error('useProductListStore must be used within a ProductListProvider');
-        }
-        return useZustandStore(store, selector, equalityFn);
-    };
+export const createContext = <
+  State,
+  Store extends StoreApi<State> = StoreApi<State>
+>() => {
+  const StoreContext = createReactContext<Store | null>(null);
+  const useStore = <T>(
+    selector: (state: State) => T,
+    equalityFn?: (left: T, right: T) => boolean
+  ) => {
+    const store = useContext(StoreContext);
+    if (!store) {
+      throw new Error(
+        "useProductListStore must be used within a ProductListProvider"
+      );
+    }
+    return useZustandStore(store, selector, equalityFn);
+  };
 
-    const Provider: React.FC<{ createStore: () => Store; children?: React.ReactNode }> = ({
-        createStore,
-        children,
-    }) => {
-        const store = useRef(createStore()).current;
-        return createElement(StoreContext.Provider, { value: store }, children);
-    };
-    return {
-        Provider,
-        useStore,
-    };
+  const Provider: React.FC<{
+    createStore: () => Store;
+    children?: React.ReactNode;
+  }> = ({ createStore, children }) => {
+    const store = useRef(createStore()).current;
+    return createElement(StoreContext.Provider, { value: store }, children);
+  };
+  return {
+    Provider,
+    useStore,
+  };
 };
